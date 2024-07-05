ce36873959163a51e96ab85d19fb7ae29390b4eb
jskadomoto <jonesseiji9@gmail.com>
2024-07-05 00:05:00 -0300

    feat(product-model): create product result model

--- src/models/ProductModel.cs
+++ src/models/ProductModel.cs
@@ -5,3 +5,16 @@ public class Product
   public required string Code { get; set; }
   public required string Name { get; set; }
 }
+
+
+public class ProductResult
+{
+  public Product? Product { get; set; }
+  public string Message { get; set; }
+
+  public ProductResult(Product? product, string message)
+  {
+    Product = product;
+    Message = message;
+  }
+}
