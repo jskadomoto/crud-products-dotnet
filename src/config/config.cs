759b611ea522c486c354acebe2f236941810588d
jskadomoto <jonesseiji9@gmail.com>
2024-08-06 22:35:57 -0300

    feat: prevent null

--- src/config/config.cs
+++ src/config/config.cs
@@ -11,5 +11,9 @@ public static class Config
     Configuration = new ConfigurationBuilder().SetBasePath
     (AppContext.BaseDirectory).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
   }
-  public static string GetConnectionString(string name) => Configuration.GetConnectionString(name);
+  public static string GetConnectionString(string name)
+  {
+    return Configuration.GetConnectionString(name ?? "DefaultConnection")
+           ?? throw new InvalidOperationException($"Connection string '{name ?? "DefaultConnection"}' not found.");
+  }
 }
