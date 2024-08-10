8048da495fd73786450e2c5cc82166862dc17432
jskadomoto <jonesseiji9@gmail.com>
2024-08-09 22:09:56 -0300

    add migrations

--- src/models/ProductModel.cs
+++ src/models/ProductModel.cs
@@ -5,6 +5,7 @@ public class Product
   public int Id { get; set; }
   public required string Code { get; set; }
   public required string Name { get; set; }
+  public string Description { get; set; }
 }
 
 
