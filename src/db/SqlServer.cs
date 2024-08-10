bc64d507b1711233541c59cecbe41cc521e452a3
jskadomoto <jonesseiji9@gmail.com>
2024-08-09 22:14:18 -0300

    feat: config table fields

--- src/db/SqlServer.cs
+++ src/db/SqlServer.cs
@@ -4,6 +4,13 @@ public class ApplicationDBContext : DbContext
 {
   public DbSet<Product> Products { get; set; }
 
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    builder.Entity<Product>().Property(product => product.Description).HasMaxLength(500).IsRequired(false);
+    builder.Entity<Product>().Property(product => product.Name).HasMaxLength(160).IsRequired();
+    builder.Entity<Product>().Property(product => product.Code).HasMaxLength(20).IsRequired();
+  }
+
   protected override void OnConfiguring(DbContextOptionsBuilder options)
   {
     var connectionString = Config.GetConnectionString("DefaultConnection");
